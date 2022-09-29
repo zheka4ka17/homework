958700a4285e9b65617892f36652d82cb868f5d7
Fenix <zheka4ka17@gmail.com>
2022-09-29 22:53:25 +0300

    message

---
+++ src/main/java/com/example/Homework.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Homework {
+    public static void main(String[] args) {
+        String line1 = "The Show must go on!";
+        String line2 = "The Show must go on! Yeah,yeah!";
+        System.out.println(line1);
+        System.out.println(line2);
+    }
+}
